a49413b51855c3a398d9739f0f7412b89851dbe5
brosenthal3 <50952285+brosenthal3@users.noreply.github.com>
2019-07-24 12:08:50 +0200

    New image, optimization

--- ui.js
+++ ui.js
@@ -1,21 +1,30 @@
-function setHeight(){
+//function that sets the height of the into element
+window.onload = function setHeight(){
     const intro = document.getElementById('intro');
+    //the height of the navbar
     const navbarHeight = document.getElementById('navbar').offsetHeight;
+    //the viewheight
     const pageHeight = window.innerHeight;
+    //set the height to the pageheight - navbar height
     intro.style.height = pageHeight - navbarHeight + "px";
   }
-  setHeight();
-  document.onclick = setHeight();
 
-  window.onscroll = () => {
+  //when the user scrolls, document the scrolling position
+  window.addEventListener('scroll', () => {
+    //the back to top (btt) button
     let btt = document.getElementById('btt');
+    //the position of the about section
     let aboutPos = document.getElementById('about').offsetTop;
+    //the current scrolling position for safari&chrome&other browsers.
     let scrollPos = document.documentElement.scrollTop;
-
-    if(scrollPos >= aboutPos){
-      btt.style.display="block";
+    let safariScrollPos = document.body.scrollTop;
+    //if you scrolled past the about section
+    if(scrollPos >= aboutPos || safariScrollPos >= aboutPos){
+        //display the btt button
+        btt.style.display="block";
     }else{
-      btt.style.display="none";
+        //hide the btt button
+        btt.style.display="none";
     }
 
-  }
+  });
