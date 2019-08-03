4c3ee7f67ff606b22385f2506ab4f721466f0e6d
brosenthal3 <50952285+brosenthal3@users.noreply.github.com>
2019-08-03 13:02:20 +0200

    small css changes

--- ui.js
+++ ui.js
@@ -1,36 +1,39 @@
 //function that sets the height of the into element
-window.onload = function setHeight(){
-    const intro = document.getElementById('intro');
-    //the height of the navbar
-    const navbarHeight = document.getElementById('navbar').offsetHeight;
-    //the viewheight
-    const pageHeight = window.innerHeight;
-    //set the height to the pageheight - navbar height
-    intro.style.height = pageHeight - navbarHeight + "px";
+function setHeight(){
+  const intro = document.getElementById('intro');
+  //the height of the navbar
+  const navbarHeight = document.getElementById('navbar').offsetHeight;
+  //the viewheight
+  const pageHeight = window.innerHeight;
+  //set the height to the pageheight - navbar height
+  intro.style.height = pageHeight - navbarHeight + "px";
+}
+
+//when the user scrolls, document the scrolling position
+window.addEventListener('scroll', () => {
+  //the back to top (btt) button
+  let btt = document.getElementById('btt');
+  //the position of the about section
+  let aboutPos = document.getElementById('about').offsetTop;
+  //the current scrolling position for safari&chrome&other browsers.
+  let scrollPos = document.documentElement.scrollTop;
+  let safariScrollPos = document.body.scrollTop;
+  //if you scrolled past the about section
+  if(scrollPos >= aboutPos || safariScrollPos >= aboutPos){
+      //display the btt button
+      btt.style.display="block";
+  }else{
+      //hide the btt button
+      btt.style.display="none";
   }
+});
 
-  //when the user scrolls, document the scrolling position
-  window.addEventListener('scroll', () => {
-    //the back to top (btt) button
-    let btt = document.getElementById('btt');
-    //the position of the about section
-    let aboutPos = document.getElementById('about').offsetTop;
-    //the current scrolling position for safari&chrome&other browsers.
-    let scrollPos = document.documentElement.scrollTop;
-    let safariScrollPos = document.body.scrollTop;
-    //if you scrolled past the about section
-    if(scrollPos >= aboutPos || safariScrollPos >= aboutPos){
-        //display the btt button
-        btt.style.display="block";
-    }else{
-        //hide the btt button
-        btt.style.display="none";
-    }
+window.addEventListener('click', setHeight());
+window.addEventListener('load', setHeight());
 
-  });
+//the function that gets activated when the back to top buttn is pressed.
+function toTop() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
 
-  //the function that gets activated when the back to top buttn is pressed.
-  function toTop() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0;
-  }
